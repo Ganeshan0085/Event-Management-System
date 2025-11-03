0254c4f0a5063963522577686de3ca71cedde1da
Ganeshan0085 <ganeshan.240085@aids.ritchennai.edu.in>
2025-11-03 19:21:36 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class DBConnection {
+    static Connection con;
+
+    public static Connection getConnection() {
+        try {
+            if (con == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/eventdb", "root", "your_password"
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
